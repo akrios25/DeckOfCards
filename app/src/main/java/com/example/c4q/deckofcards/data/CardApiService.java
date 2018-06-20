46fb9d56674de605af4489f649cde5a75a04da5a
kevin toro <kevintoro@ac.c4q.nyc>
2018-06-20 19:26:08 -0400

    intialized repository

---
+++ app/src/main/java/com/example/c4q/deckofcards/data/CardApiService.java
@@ -0,0 +1,8 @@
+package com.example.c4q.deckofcards.data;
+
+/**
+ * Created by c4q on 6/20/18.
+ */
+
+public interface CardApiService {
+}
---
+++ app/src/main/java/com/example/c4q/deckofcards/model/Cards.java
@@ -0,0 +1,8 @@
+package com.example.c4q.deckofcards.model;
+
+/**
+ * Created by c4q on 6/20/18.
+ */
+
+public class Cards {
+}
