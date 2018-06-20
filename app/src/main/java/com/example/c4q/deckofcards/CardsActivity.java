c6adfbb7e1a45312294da19b4555f7f5003af12f
kevin toro <kevintoro@ac.c4q.nyc>
2018-06-20 19:51:58 -0400

    Card Activity Layout

--- app/src/main/java/com/example/c4q/deckofcards/CardsActivity.java
+++ app/src/main/java/com/example/c4q/deckofcards/CardsActivity.java
@@ -2,12 +2,27 @@ package com.example.c4q.deckofcards;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class CardsActivity extends AppCompatActivity {
+    private Button shuffleDeck;
+    private Button drawCards;
+    private EditText numOfCards;
+    private TextView remainingCards;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cards);
+        setViews();
+    }
+    private void setViews() {
+        shuffleDeck = findViewById(R.id.shuffle_button);
+        drawCards = findViewById(R.id.draw_button);
+        numOfCards = findViewById(R.id.how_many_cards);
+        remainingCards = findViewById(R.id.cards_remaining);
+
     }
 }
