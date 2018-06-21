bb78278dfaaab9f698a83453213aea9d0b32d326
kevin toro <kevintoro@ac.c4q.nyc>
2018-06-20 20:24:18 -0400

    Card Adapter created

--- app/src/main/java/com/example/c4q/deckofcards/presenter/CardsAdapter.java
+++ app/src/main/java/com/example/c4q/deckofcards/presenter/CardsAdapter.java
@@ -1,8 +1,61 @@
 package com.example.c4q.deckofcards.presenter;
 
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.example.c4q.deckofcards.R;
+import com.example.c4q.deckofcards.model.Cards;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
 /**
  * Created by c4q on 6/20/18.
  */
 
-public class CardsAdapter {
+public class CardsAdapter extends RecyclerView.Adapter<CardsAdapter.CardViewHolder>{
+    List<Cards> cardsList;
+
+    public CardsAdapter(List<Cards> cardsList){
+        this.cardsList = cardsList;
+    }
+
+    @NonNull
+    @Override
+    public CardsAdapter.CardViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.card_item, parent, false);
+        return new CardViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CardsAdapter.CardViewHolder holder, int position) {
+        Cards cards = cardsList.get(position);
+        holder.onBind(cards);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return cardsList.size();
+    }
+    public void addCards(List<Cards> cards){
+        cardsList.addAll(cards);
+    }
+    public static class CardViewHolder extends RecyclerView.ViewHolder {
+        private ImageView cardImageView;
+
+        public CardViewHolder(View itemview){
+            super(itemview);
+            cardImageView = itemview.findViewById(R.id.card_image);
+        }
+        public void onBind(Cards cards){
+            Picasso.with(itemView.getContext())
+                    .load(cards.getImage())
+                    .into(cardImageView);
+        }
+    }
 }
